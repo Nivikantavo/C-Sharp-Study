a16193afc55ac497a8d97a5fb109078137365b2c
Nivikantavo <63458816+Nivikantavo@users.noreply.github.com>
2020-04-11 10:25:06 +0300

    Adding unity project with task "alarm ageinst rogue"

---
+++ Assets/Scripts/Signaling.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Signaling : MonoBehaviour
+{
+    [SerializeField] private AudioClip _signaling;
+    AudioSource audioSource;
+    private bool _isSignaling = false;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void Update()
+    {
+        if(_isSignaling)
+            audioSource.volume += Time.deltaTime;
+        else
+            audioSource.volume -= Time.deltaTime;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Player>(out Player player))
+        {
+            _isSignaling = !_isSignaling;
+            if(_isSignaling)
+                audioSource.PlayOneShot(_signaling);
+            if (audioSource.volume == 0 && !_isSignaling)
+                audioSource.Stop();
+        }
+    }
+}
